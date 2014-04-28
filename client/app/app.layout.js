adfd45d99756bbe6809baf822fb057f793affbce
Abigail Watson <awatson1978@gmail.com>
2014-04-28 01:01:51 -0400

    initial commit

---
+++ client/app/app.layout.js
@@ -0,0 +1,116 @@
+//--------------------------------------------------------------
+// Layout Resizing
+
+
+UI.body.resized = function(){
+
+  $('#westPanel').sidebar();
+  $('#eastPanel')
+    .sidebar({overlay: false});
+
+  $('#errorPanel').sidebar();
+  $('#contextPanel').sidebar();
+
+  return Session.get('resize');
+};
+
+
+//--------------------------------------------------------------
+// Template.errorPanel
+
+Template.errorPanel.getErrorMessage = function(){
+  return "Error Message!";
+};
+
+
+//--------------------------------------------------------------
+// Routing Layouts
+
+Router.configure({
+  layoutTemplate: 'appLayout',
+  notFoundTemplate: 'notFoundPage'
+});
+Router.onBeforeAction(function() {
+  if (!Meteor.loggingIn() && !Meteor.user()) {
+    this.redirect('/sign-in');
+  }
+}, {
+  except: [
+    'landingRoute',
+    'entrySignUpRoute',
+    'entrySignInRoute',
+    'eulaRoute',
+    'privacyRoute',
+    'aboutRoute',
+    'glossaryRoute',
+    'browserNotSupportedRoute',
+    'pageNotFoundRoute',
+    'entryForgotPasswordRoute',
+    'entrySignOutRoute',
+    'entryResetPasswordRoute'
+  ]
+});
+Router.onBeforeAction(function() {
+  if(!bowser.webkit){
+    this.render('browserNotSupportedPage');
+    this.pause();
+  }
+});
+
+
+//--------------------------------------------------------------
+// Routing Layout Helper Functions
+
+setPageTitle = function(newTitle) {
+  document.title = newTitle;
+};
+checkBrowserIsSupported = function(scope) {
+  console.log('checkBrowserIsSupported');
+  if(!bowser.webkit){
+    scope.render('browserNotSupportedPage');
+    scope.pause();
+  }
+};
+
+checkUserHasEmployer = function(scope) {
+  console.log('checkUserHasEmployer');
+  if (Meteor.user()) {
+    if (!Meteor.user().profile.employer_id) {
+      scope.render("noEmployerSetPageErrorPage");
+
+      scope.render("navbarHeader", {to: 'header'});
+      //scope.render("sidebarTemplate",{to: 'aside'});
+      scope.pause();
+    } else {
+      scope.render("navbarHeader", {to: 'header'});
+      //scope.render("sidebarTemplate",{to: 'aside'});
+      scope.pause();
+    }
+  }
+};
+
+getYieldTemplates = function() {
+  if (Meteor.userId()) {
+    return {
+      'navbarHeader': {
+        to: 'header'
+      },
+      'navbarFooter': {
+        to: 'footer'
+      }
+      //'sidebarTemplate': {to: 'aside'}
+    };
+  } else {
+    return {
+      'navbarHeader': {
+        to: 'header'
+      },
+      'navbarFooter': {
+        to: 'footer'
+      }
+
+      //'sidebarTemplate': {to: 'aside'}
+    };
+    //return {};
+  }
+};
---
+++ client/app/navbars/navbarHeader.js
@@ -0,0 +1,30 @@
+
+// ---------------------------------------------------------------
+// template events
+
+Template.navbarHeader.events({
+  'click #navbarBrandLink':function(){
+    $('#westPanel').sidebar('toggle');
+  },
+  'click #logOutLink':function(){
+    Router.go('/sign-out');
+  }
+});
+
+
+
+// ---------------------------------------------------------------
+// template helpers
+
+Template.navbarHeader.getUserName = function(){
+  if(Meteor.userId()){
+    if(Meteor.user()){
+      //return Meteor.user().emails[0].address;
+      return Meteor.user().username;
+    }else{
+      return "---";
+    }
+  }else{
+    return "Sign In";
+  }
+};
