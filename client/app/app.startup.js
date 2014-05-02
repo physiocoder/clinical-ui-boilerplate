86751cbee16da61b34f06acfeab4032c0c497b9a
Abigail Watson <awatson1978@gmail.com>
2014-05-02 08:22:36 -0400

    removed environment detection

--- client/app/app.startup.js
+++ client/app/app.startup.js
@@ -23,30 +23,3 @@ removeWallpaper = function(){
   console.log('removing wallpaper...');
   $('html').removeClass('landscapeLogin');
 };
-
-
-
-
-Meteor.startup(function(){
-  // replace getEnvironment
-  Meteor.call('getEnvironmentRoot', function(error, result){
-    if(error){
-      console.log(error);
-    }
-    if(result){
-      console.log('current environment url:  ' + result);
-      if(result === "http://insights.thinaire.net"){
-        // load production config
-        //Session.setDefault('apiAddressAndPort', Meteor.settings.production.apiAddress);
-        Session.setDefault('apiAddressAndPort', 'http://api.thinaire.net');
-      }else{
-        // load dev config
-        //Session.setDefault('apiAddressAndPort', Meteor.settings.development.apiAddress);
-        Session.setDefault('apiAddressAndPort', 'http://tadev01:8889');
-      }
-
-    }
-  });
-
-
-});
